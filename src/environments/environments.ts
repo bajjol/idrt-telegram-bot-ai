7f244f0a499c23a23f38c5bcaf765182e38ee2d0
tekumpreng1 <tekumpreng1@gmail.com>
2024-10-03 08:04:24 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyDnPa47DxooMjq-Qmle95vWK3WkZmd-gqU',
+      authDomain: 'idrt-telegram-bot-fc75b.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'idrt-telegram-bot-fc75b',
+      storageBucket: 'idrt-telegram-bot-fc75b.appspot.com',
+      messagingSenderId: '645730712922',
+      appId: '1:645730712922:web:d14dae4a19605100d90763',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyAEbForU2gpujk08z9Qf1ff3rf7juaz9CM',
+  debug_token: ''
+};
